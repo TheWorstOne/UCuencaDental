e6ee9c653f05024d45530d536f5104e17b974eec
TheWorstOne <mangelladen@gmail.com>
2019-06-08 23:45:04 -0500

    Adding general product table :sparkles:

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -14,20 +14,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class TableListComponent implements OnInit {
 
-  displayedColumns = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns = ['Id', 'Nombre', 'Descripcion', 'Stock'];
+  dataSource: MatTableDataSource<Producto>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
 
   constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) {
-    // Create 100 users
-    const users: UserData[] = [];
-    for (let i = 1; i <= 100; i++) { users.push(createNewUser(i)); }
 
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.dataSource = new MatTableDataSource();
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.getProducts()
   }
 
   ngOnInit() {
@@ -48,32 +48,22 @@ export class TableListComponent implements OnInit {
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
+
+  getProducts() {
+    this.rest.getProducts().subscribe((data: {}) => {
+      this.dataSource.data = data['data']
+    });
+  }
+
 }
 
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
-}
-
-/** Constants used to fill up our data base. */
-const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
+export interface Producto {
+  Id: string;
+  Nombre: string;
+  Descripcion: string;
+  PrecioUnitario: number;
+  Stock: number;
+  Activo: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
